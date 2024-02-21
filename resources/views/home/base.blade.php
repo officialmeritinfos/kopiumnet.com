e331781421ef2b94c3027dfbc86bf3e0f99f99fe
Anon Dev <michaelerastus9@gmail.com>
2024-02-21 01:23:53 +0100

    Added Livechat

--- resources/views/home/base.blade.php
+++ resources/views/home/base.blade.php
@@ -284,6 +284,18 @@
 <!-- main -->
 <script src="{{asset('home/js/main.js')}}"></script>
 
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/65d5427e8d261e1b5f62e6f8/1hn4gng32';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>
